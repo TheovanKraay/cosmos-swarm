43f8c021cfff59887ef0afe05b1f982c947fa2ed
Theo van Kraay <theo.van@microsoft.com>
2024-12-07 17:11:27 +0000

    refine

--- main.py
+++ main.py
@@ -90,15 +90,17 @@ def order_item(user_id, product_id):
             cosmosdb.add_purchase(int(user_id), date_of_purchase, item_id, price)
         else:
             print(f"Product {product_id} not found.")
+        return "item_id: " + str(item_id)
     except Exception as e:
         print(f"An error occurred during order placement: {e}")
 
-def product_information_chat(user_prompt):
-    # Perform a vector search on the Cosmos DB container
+def product_information(user_prompt):
+    """Provide information about a product based on the user prompt.
+    Takes as input the user prompt as a string."""
+    # Perform a vector search on the Cosmos DB container and return results to the agent
     vectors = cosmosdb.generate_embedding(user_prompt)
     vector_search_results = vector_search(cosmosdb.products_container_name, vectors)
-    results = generate_completion(user_prompt, vector_search_results, chat_history=[])
-    return results
+    return vector_search_results
 
 # Perform a vector search on the Cosmos DB container
 def vector_search(container, vectors, similarity_score=0.02, num_results=3):
@@ -124,52 +126,14 @@ def vector_search(container, vectors, similarity_score=0.02, num_results=3):
     formatted_results = []
     for result in results:
         score = result.pop('SimilarityScore')
-        #convert product_id to string
         result['product_id'] = str(result['product_id'])
-        #apend product id to the beginning of each product description
         result['product_description'] ="product id "+result['product_id'] + ": " + result['product_description']
         formatted_result = {
             'SimilarityScore': score,
             'document': result
         }
-        #print(formatted_result)
         formatted_results.append(formatted_result)
     return formatted_results
-
-
-def generate_completion(user_prompt, vector_search_results, chat_history):
-    system_prompt = '''
-    You are an intelligent assistant for product information. You are designed to provide helpful answers to user questions about products in the database.
-    You are friendly, helpful, and informative and can be lighthearted. Be concise in your responses, but still friendly.
-        - Only answer questions related to the information provided below.
-        - Write two lines of whitespace between each answer in the list.
-    '''
-
-    # Create a list of messages as a payload to send to the OpenAI Completions API
-
-    # system prompt
-    messages = [{'role': 'system', 'content': system_prompt}]
-
-    # user prompt
-    messages.append({'role': 'user', 'content': user_prompt})
-
-    # vector search results
-    for result in vector_search_results:
-        messages.append({'role': 'system', 'content': json.dumps(result['document'])})
-
-    # print("Messages going to openai", messages)
-    # Create the completion
-    response = aoai_client.chat.completions.create(
-        model=openai_completions_deployment,
-        messages=messages,
-        temperature=0.1
-    )
-    #print("Response to product query: \n")
-    completions_results = response.model_dump()
-
-    #print(completions_results['choices'][0]['message']['content'])
-    return completions_results['choices'][0]['message']['content']
-
 
 
 # Initialize the database
@@ -183,54 +147,76 @@ cosmosdb.preview_table("Products")
 def transfer_to_sales():
     return sales_agent
 
-
 def transfer_to_refunds():
     return refunds_agent
+
+def transfer_to_product():
+    return product_agent
+
+def transfer_to_triage():
+    return triage_agent
 
 # Define the agents
 
 refunds_agent = Agent(
     name="Refunds Agent",
-    description="""You are a refund agent that handles all actions related to refunds after a return has been processed.
-    You must ask for both the user ID and item ID to initiate a refund. Ask for both user_id and item_id in one message.
+    instructions="""You are a refund agent that handles all actions related to refunds after a return has been processed.
+    You must ask for both the user ID and item ID to initiate a refund. 
+    If item_id is present in the context information, use it. 
+    Otherwise, do not make any assumptions, you must ask for the item ID as well.
+    Ask for both user_id and item_id in one message.
     Do not use any other context information to determine whether the right user id or item id has been provided - just accept the input as is.
     If the user asks you to notify them, you must ask them what their preferred method of notification is. For notifications, you must
-    ask them for user_id and method in one message.""",
-    functions=[refund_item, notify_customer],
+    ask them for user_id and method in one message.
+    If the user asks you a question you cannot answer, transfer back to the triage agent.""",
+    functions=[transfer_to_triage, refund_item, notify_customer],
 )
 
 sales_agent = Agent(
     name="Sales Agent",
-    description="""You are a sales agent that handles all actions related to placing an order to purchase an item.
-    Regardless of what the user wants to purchase, must ask for BOTH the user ID and product ID to place an order.
+    instructions="""You are a sales agent that handles all actions related to placing an order to purchase an item.
+    Regardless of what the user wants to purchase, must ask for the user ID. 
+    If the product id is present in the context information, use it. Otherwise, you must as for the product ID as well.
     An order cannot be placed without these two pieces of information. Ask for both user_id and product_id in one message.
     If the user asks you to notify them, you must ask them what their preferred method is. For notifications, you must
     ask them for user_id and method in one message.
+    If the user asks you a question you cannot answer, transfer back to the triage agent.""",
+    functions=[transfer_to_triage, order_item, notify_customer, transfer_to_refunds],
+)
+
+product_agent = Agent(
+    name="Product Agent",
+    instructions="""You are a product agent that provides information about products in the database.
+    When calling the product_information function, do not make any assumptions 
+    about the product id, or number the products in the response. Instead, use the product id from the response to 
+    product_information and align that product id whenever referring to the corresponding product in the database. 
+    Only give the user very basic information about the product; the product name, id, and very short description.
+    If the user asks for more information about any product, provide it. 
+    If the user asks you a question you cannot answer, transfer back to the triage agent.
     """,
-    agents=[refunds_agent],
-    functions=[order_item, notify_customer, transfer_to_refunds],
+    functions=[transfer_to_triage, product_information, transfer_to_sales, transfer_to_refunds],
+    add_backlinks=True,
 )
 
 triage_agent = Agent(
     name="Triage Agent",
     instructions="""You are to triage a users request, and call a tool to transfer to the right intent.
-    If the user asks for product information, you should call the product_information_chat function, 
-    passing their question to it, and returning the results.
     Otherwise, once you are ready to transfer to the right intent, call the tool to transfer to the right intent.
     You dont need to know specifics, just the topic of the request.
+    If the user asks for product information, transfer to the Product Agent.
     If the user request is about making an order or purchasing an item, transfer to the Sales Agent.
     If the user request is about getting a refund on an item or returning a product, transfer to the Refunds Agent.
     When you need more information to triage the request to an agent, ask a direct question without explaining why you're asking it.
     Do not share your thought process with the user! Do not make unreasonable assumptions on behalf of user.""",
-    agents=[sales_agent, refunds_agent],
-    functions=[product_information_chat],
+    agents=[sales_agent, refunds_agent, product_agent],
+    functions=[transfer_to_sales, transfer_to_refunds, transfer_to_product],
     add_backlinks=True,
 )
 
 for f in triage_agent.functions:
     print(f.__name__)
 
-triage_agent.functions = [transfer_to_sales, transfer_to_refunds, product_information_chat]
+triage_agent.functions = [transfer_to_sales, transfer_to_refunds, transfer_to_product]
 
 if __name__ == "__main__":
     # Run the demo loop
